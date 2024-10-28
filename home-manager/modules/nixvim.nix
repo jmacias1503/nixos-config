60017e1918ce5e22f8b380e4de2492e30f914874
Alejandro Macias <alex.mac.fon@gmail.com>
2024-10-28 07:30:45 -0600

    feat(nixvim): added cmp

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -19,5 +19,15 @@
 	lua_ls.enable = true;
       };
     };
+
+    plugins.cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings.sources = [
+	{ name = "nvim_lsp"; }
+	{ name = "path"; }
+	{ name = "buffer"; }
+      ];
+    };
   };
 }
