454c6979df4800f95f16fe6b23786e19a64677dd
Alejandro Macias <alex.mac.fon@gmail.com>
2024-11-02 11:04:17 -0600

    feat: added completion mappings

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -26,6 +26,18 @@
 	{ name = "nvim_lsp"; }
 	{ name = "path"; }
 	];
+	settings.mapping = {
+	  "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+	  "<C-j>" = "cmp.mapping.select_next_item()";
+	  "<C-k>" = "cmp.mapping.select_prev_item()";
+	  "<C-e>" = "cmp.mapping.abort()";
+	  "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+	  "<C-f>" = "cmp.mapping.scroll_docs(4)";
+	  "<C-Space>" = "cmp.mapping.complete()";
+	  "<CR>" = "cmp.mapping.confirm({ select = true })";
+	  "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+	};
+	};
       };
       lsp = {
 	enable = true;
