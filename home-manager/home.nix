628a4b05effca693b72fbb686b9881ecd7143fb8
Alejandro Macias <alex.mac.fon@gmail.com>
2024-11-03 23:08:06 -0600

    refactor: moved packages to their own module

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,39 +1,12 @@
-{pkgs, ...}:
+{...}:
 {
   imports = [
     ./modules/shell.nix
     ./modules/nixvim.nix
     ./modules/programs-config.nix
+    ./modules/packages.nix
   ];
 
-  nixpkgs.config.allowUnfree = true;
-  home.packages  = with pkgs; [
-    bluedevil
-    cmus
-    fastfetch
-    gimp
-    imv
-    inkscape
-    libreoffice
-    mpv
-    ncpamixer
-    qrcp
-    rpi-imager
-    signal-desktop
-    slack
-    (texlive.combine {
-      inherit (pkgs.texlive)
-      scheme-full
-      pgf
-      ;
-    })
-    ttyper
-    whatsapp-for-linux
-    ytfzf
-    zathura
-    zoom-us
-    zsh
-  ];
   home.sessionVariables = {
     EDITOR = "nvim";
     BROWSER = "brave";
---
+++ home-manager/modules/packages.nix
@@ -0,0 +1,31 @@
+{pkgs, ...}:
+{
+  nixpkgs.config.allowUnfree = true;
+  home.packages  = with pkgs; [
+    bluedevil
+    cmus
+    fastfetch
+    gimp
+    imv
+    inkscape
+    libreoffice
+    mpv
+    ncpamixer
+    qrcp
+    rpi-imager
+    signal-desktop
+    slack
+    (texlive.combine {
+      inherit (pkgs.texlive)
+      scheme-full
+      pgf
+      ;
+    })
+    ttyper
+    whatsapp-for-linux
+    ytfzf
+    zathura
+    zoom-us
+    zsh
+  ];
+}
