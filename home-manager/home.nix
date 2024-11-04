ec6109dd0b9f76f7e224a1e5e33bce07345279fd
Alejandro Macias <alex.mac.fon@gmail.com>
2024-11-03 22:58:06 -0600

    refactor: moved programs configuration to own module

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,6 +3,7 @@
   imports = [
     ./modules/shell.nix
     ./modules/nixvim.nix
+    ./modules/programs-config.nix
   ];
 
   nixpkgs.config.allowUnfree = true;
@@ -33,22 +34,6 @@
     zoom-us
     zsh
   ];
-  programs = {
-    cmus = {
-      theme = "dracula";
-    };
-    git = {
-      enable = true;
-      userName = "Alejandro jmacias8075";
-      userEmail = "alex.mac.fon@gmail.com";
-      aliases = {
-        lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
-      };
-    };
-    gpg = {
-      enable = true;
-    };
-  };
   home.sessionVariables = {
     EDITOR = "nvim";
     BROWSER = "brave";
