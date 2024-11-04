8248088d50a51dfb74cadd73ffb7d13d5285f9db
Alejandro Macias <alex.mac.fon@gmail.com>
2024-11-03 22:06:13 -0600

    feat: added allowed site list

--- nixos/modules/browser.nix
+++ nixos/modules/browser.nix
@@ -45,6 +45,29 @@
 				"[*.]slack.com"
 				"[*.]uaqedvirtual.uaq.mx"
 			];
+			"URLAllowlist" = [
+				"[*.]app.diagrams.net"
+				"[*.]canva.com"
+				"[*.]chatgpt.com"
+				"[*.]chromeenterprise.google"
+				"[*.]coursera.org"
+				"[*.]dbdiagram.io"
+				"[*.]dndbeyond.com"
+				"[*.]github.com"
+				"[*.]google.com"
+				"[*.]meet.jit.si"
+				"[*.]nestjs.com"
+				"[*.]nixos.org"
+				"[*.]nix-community[*.]"
+				"[*.]notion.so"
+				"[*.]openstreetmap.org"
+				"[*.]prisma.io"
+				"[*.]slack.com"
+				"[*.]uaqedvirtual.uaq.mx"
+			];
+			"URLBlocklist" = [
+			  "*"
+			];
 		};
 	};
 }
