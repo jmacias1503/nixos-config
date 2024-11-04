df2bdc372fe65381195c124932f5092629cd7410
Alejandro Macias <alex.mac.fon@gmail.com>
2024-11-03 22:07:01 -0600

    style: tabs

--- nixos/modules/browser.nix
+++ nixos/modules/browser.nix
@@ -1,73 +1,73 @@
 {...}:
 
 {
-	programs.chromium = {
-		enable = true;
-		extensions = [
-			"inmopeiepgfljkpkidclfgbgbmfcennb"
-			"dlnejlppicbjfcfcedcflplfjajinajd"
-		];
-		extraOpts = {
-			"CookiesAllowedForUrls" = [
-				"[*.]canva.com"
-				"[*.]chatgpt.com"
-				"[*.]chromeenterprise.google"
-				"[*.]coursera.org"
-				"[*.]dbdiagram.io"
-				"[*.]dndbeyond.com"
-				"[*.]github.com"
-				"[*.]google.com"
-				"[*.]notion.so"
-				"[*.]prisma.io"
-				"[*.]slack.com"
-				"[*.]uaqedvirtual.uaq.mx"
-			];
-			"DefaultCookiesSetting" = 2;
-			"DefaultJavaScriptSetting" = 2;
-			"JavaScriptAllowedForUrls" = [
-				"192.168.10.1"
-				"[*.]app.diagrams.net"
-				"[*.]canva.com"
-				"[*.]chatgpt.com"
-				"[*.]chromeenterprise.google"
-				"[*.]coursera.org"
-				"[*.]dbdiagram.io"
-				"[*.]dndbeyond.com"
-				"[*.]github.com"
-				"[*.]google.com"
-				"[*.]meet.jit.si"
-				"[*.]nestjs.com"
-				"[*.]nixos.org"
-				"[*.]nix-community[*.]"
-				"[*.]notion.so"
-				"[*.]openstreetmap.org"
-				"[*.]prisma.io"
-				"[*.]slack.com"
-				"[*.]uaqedvirtual.uaq.mx"
-			];
-			"URLAllowlist" = [
-				"[*.]app.diagrams.net"
-				"[*.]canva.com"
-				"[*.]chatgpt.com"
-				"[*.]chromeenterprise.google"
-				"[*.]coursera.org"
-				"[*.]dbdiagram.io"
-				"[*.]dndbeyond.com"
-				"[*.]github.com"
-				"[*.]google.com"
-				"[*.]meet.jit.si"
-				"[*.]nestjs.com"
-				"[*.]nixos.org"
-				"[*.]nix-community[*.]"
-				"[*.]notion.so"
-				"[*.]openstreetmap.org"
-				"[*.]prisma.io"
-				"[*.]slack.com"
-				"[*.]uaqedvirtual.uaq.mx"
-			];
-			"URLBlocklist" = [
-			  "*"
-			];
-		};
-	};
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      "inmopeiepgfljkpkidclfgbgbmfcennb"
+      "dlnejlppicbjfcfcedcflplfjajinajd"
+    ];
+    extraOpts = {
+      "CookiesAllowedForUrls" = [
+	"[*.]canva.com"
+	"[*.]chatgpt.com"
+	"[*.]chromeenterprise.google"
+	"[*.]coursera.org"
+	"[*.]dbdiagram.io"
+	"[*.]dndbeyond.com"
+	"[*.]github.com"
+	"[*.]google.com"
+	"[*.]notion.so"
+	"[*.]prisma.io"
+	"[*.]slack.com"
+	"[*.]uaqedvirtual.uaq.mx"
+      ];
+      "DefaultCookiesSetting" = 2;
+      "DefaultJavaScriptSetting" = 2;
+      "JavaScriptAllowedForUrls" = [
+	"192.168.10.1"
+	"[*.]app.diagrams.net"
+	"[*.]canva.com"
+	"[*.]chatgpt.com"
+	"[*.]chromeenterprise.google"
+	"[*.]coursera.org"
+	"[*.]dbdiagram.io"
+	"[*.]dndbeyond.com"
+	"[*.]github.com"
+	"[*.]google.com"
+	"[*.]meet.jit.si"
+	"[*.]nestjs.com"
+	"[*.]nixos.org"
+	"[*.]nix-community[*.]"
+	"[*.]notion.so"
+	"[*.]openstreetmap.org"
+	"[*.]prisma.io"
+	"[*.]slack.com"
+	"[*.]uaqedvirtual.uaq.mx"
+      ];
+      "URLAllowlist" = [
+	"[*.]app.diagrams.net"
+	"[*.]canva.com"
+	"[*.]chatgpt.com"
+	"[*.]chromeenterprise.google"
+	"[*.]coursera.org"
+	"[*.]dbdiagram.io"
+	"[*.]dndbeyond.com"
+	"[*.]github.com"
+	"[*.]google.com"
+	"[*.]meet.jit.si"
+	"[*.]nestjs.com"
+	"[*.]nixos.org"
+	"[*.]nix-community[*.]"
+	"[*.]notion.so"
+	"[*.]openstreetmap.org"
+	"[*.]prisma.io"
+	"[*.]slack.com"
+	"[*.]uaqedvirtual.uaq.mx"
+      ];
+      "URLBlocklist" = [
+	"*"
+      ];
+    };
+  };
 }
