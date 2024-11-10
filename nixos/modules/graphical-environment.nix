0606cb28b8f51a91d53007228c874a324497cbd0
Alejandro Macias <alex.mac.fon@gmail.com>
2024-11-10 11:45:29 -0600

    style: tabs

--- nixos/modules/graphical-environment.nix
+++ nixos/modules/graphical-environment.nix
@@ -2,11 +2,11 @@
 
 {
 
-	services.xserver.enable = true;
-	services.xserver.windowManager.dwm = {
-		enable = true;
-		package = pkgs.dwm.overrideAttrs {
-			src = ./../packages/self-built/dwm;
-		};
-	};
+  services.xserver.enable = true;
+  services.xserver.windowManager.dwm = {
+    enable = true;
+    package = pkgs.dwm.overrideAttrs {
+      src = ./../packages/self-built/dwm;
+    };
+  };
 }
