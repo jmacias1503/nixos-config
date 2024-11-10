026cbf277d41e3088808864754f03fcba567c4b9
Alejandro Macias <alex.mac.fon@gmail.com>
2024-11-10 13:08:09 -0600

    fix: added bin path

--- home-manager/modules/scripts/browse.nix
+++ home-manager/modules/scripts/browse.nix
@@ -2,5 +2,5 @@
 
 pkgs.writeShellScriptBin "browse" ''
   browsing_method=$(echo "ytfzf\nbrave" | ${pkgs.dmenu}/bin/dmenu)
-  prompt=$(${pkgs.dmenu} -p "prompt:")
+  prompt=$(${pkgs.dmenu}/bin/dmenu -p "prompt:")
 ''
