9b639d424b1f79d89a1869be22142c538aef0805
Alejandro Macias <alex.mac.fon@gmail.com>
2024-11-11 13:26:17 -0600

    fix: removed concatlists

--- nixosModules/packages/default.nix
+++ nixosModules/packages/default.nix
@@ -1,11 +1,6 @@
-{lib, pkgs, ...}:{
+{...}:{
   imports = [
     ./gui-packages.nix
     ./term-packages.nix
-  ];
-
-  environment.systemPackages = with pkgs; lib.concatLists [
-    gui-packages
-    term-packages
   ];
 }
