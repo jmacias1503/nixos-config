278ce9feee1daab518fa7665543689da492a71fb
Alejandro Macias <alex.mac.fon@gmail.com>
2024-11-13 13:29:05 -0600

    feat: added init script

--- homeManagerModules/shell.nix
+++ homeManagerModules/shell.nix
@@ -1,5 +1,4 @@
-{ config, ...}:
-
+{config, ...}:
 {
   programs.zsh = {
     enable = true;
@@ -12,10 +11,11 @@
       size = 100000;
       path = "${config.xdg.dataHome}/zsh/history";
     };
+    initExtra = "< $HOME/Documents/todo-list.txt";
     shellAliases = {
       v = "nvim";
-      sb = "cd ~/Documents/second-brain";
-      chambauaq = "nix develop ~/Shells#chambauaq";
+      sb = "cd $HOME/Documents/second-brain";
+      chambauaq = "nix develop $HOME/Shells#chambauaq";
       osbuild = "sudo nixos-rebuild switch --flake $OSFLAKE/ --show-trace";
     };
     syntaxHighlighting.enable = true;
