c3b2f823b6228be98dd0b97fcbd1c32084ebcb8a
Alejandro Macias <alex.mac.fon@gmail.com>
2024-11-22 13:32:11 -0600

    fix: removed xdg default apps

--- homeUsers/jmacias8075/home.nix
+++ homeUsers/jmacias8075/home.nix
@@ -13,31 +13,5 @@
   #users.users.jmacias8075.shell = pkgs.zsh;
   home.username = "jmacias8075";
   home.stateVersion = "24.05";
-  xdg = {
-    desktopEntries = {
-      imv = {
-	name = "ImV";
-	exec = "${pkgs.imv}/bin/imv";
-      };
-      neovim = {
-	name = "NeoVim";
-	exec = "${pkgs.neovim}/bin/nvim";
-      };
-      zathura = {
-	name = "Zathura";
-	exec = "${pkgs.zathura}/bin/zathura";
-      };
-    };
-    mimeApps = {
-      enable = true;
-      defaultApplications = {
-	"text/plain" = "neovim.desktop";
-	"application/pdf" = "zathura.desktop";
-	"image/jpeg" = "imv.desktop";
-	"image/jpg" = "imv.desktop";
-	"image/png" = "imv.desktop";
-      };
-    };
-  };
   home.homeDirectory = "/home/jmacias8075";
 }
