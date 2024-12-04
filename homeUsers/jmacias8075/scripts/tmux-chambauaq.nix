0f33353f5d24002cdbc4dd39932fb8960f026bf6
Alejandro Macias <alex.mac.fon@gmail.com>
2024-12-04 12:38:22 -0600

    fix: quoted windows and added db and rest-tests windows

--- homeUsers/jmacias8075/scripts/tmux-chambauaq.nix
+++ homeUsers/jmacias8075/scripts/tmux-chambauaq.nix
@@ -4,11 +4,14 @@
     ${pkgs.tmux}/bin/tmux has-session -t $SESH 2>/dev/null
     if [ $? != 0 ]; then
       ${pkgs.tmux}/bin/tmux new-session -d -s $SESH -n "editor"
-      ${pkgs.tmux}/bin/tmux new-windonw -t $SESH -n "server"
-      ${pkgs.tmux}/bin/tmux send-keys -t $SESH:editor "cd ~/Documents/projects/bolsa_de_trabajo_back" C-m
-      ${pkgs.tmux}/bin/tmux send-keys -t $SESH:editor "${pkgs.neovim}/bin/nvim ." C-m
-      ${pkgs.tmux}/bin/tmux send-keys -t $SESH:server "cd ~/Documents/projects/bolsa_de_trabajo_back" C-m
-      ${pkgs.tmux}/bin/tmux send-keys -t $SESH:server "${pkgs.nodejs}/bin/npm run start:dev" C-m
+      ${pkgs.tmux}/bin/tmux new-window -t $SESH -n "server"
+      ${pkgs.tmux}/bin/tmux new-window -t $SESH -n "db"
+      ${pkgs.tmux}/bin/tmux new-window -t $SESH -n "rest-tests"
+      ${pkgs.tmux}/bin/tmux send-keys -t $SESH:"editor" "cd ~/Documents/projects/bolsa_de_trabajo_back" C-m
+      ${pkgs.tmux}/bin/tmux send-keys -t $SESH:"editor" "${pkgs.neovim}/bin/nvim ." C-m
+      ${pkgs.tmux}/bin/tmux send-keys -t $SESH:"server" "cd ~/Documents/projects/bolsa_de_trabajo_back" C-m
+      ${pkgs.tmux}/bin/tmux send-keys -t $SESH:"server" "${pkgs.nodejs}/bin/npm run start:dev" C-m
+      ${pkgs.tmux}/bin/tmux send-keys -t $SESH:"db" "${pkgs.mongosh}/bin/mongosh $DATABASE_URL"
     fi
 
     tmux attach-session -t $SESH
