3a735cc3f90ec76508faafe9aa6c9ad167746440
Alejandro Macias <alex.mac.fon@gmail.com>
2024-12-04 14:09:13 -0600

    feat: added env variables to tmux session

--- homeUsers/jmacias8075/scripts/tmux-chambauaq.nix
+++ homeUsers/jmacias8075/scripts/tmux-chambauaq.nix
@@ -4,6 +4,12 @@
     ${pkgs.tmux}/bin/tmux has-session -t $SESH 2>/dev/null
     if [ $? != 0 ]; then
       ${pkgs.tmux}/bin/tmux new-session -d -s $SESH -n "editor"
+      ${pkgs.tmux}/bin/tmux setenv AWS_BUCKET_NAME $AWS_BUCKET_NAME
+      ${pkgs.tmux}/bin/tmux setenv AWS_S3_ID $AWS_S3_ID
+      ${pkgs.tmux}/bin/tmux setenv AWS_S3_REGION $AWS_S3_REGION
+      ${pkgs.tmux}/bin/tmux setenv AWS_S3_SECRET_KEY $AWS_S3_SECRET_KEY
+      ${pkgs.tmux}/bin/tmux setenv DATABASE_URL $DATABASE_URL
+      ${pkgs.tmux}/bin/tmux setenv JWT_SECRET_TOKEN $JWT_SECRET_TOKEN
       ${pkgs.tmux}/bin/tmux new-window -t $SESH -n "server"
       ${pkgs.tmux}/bin/tmux new-window -t $SESH -n "db"
       ${pkgs.tmux}/bin/tmux new-window -t $SESH -n "rest-tests"
