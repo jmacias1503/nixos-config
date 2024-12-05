63efdbfefb1bca72a9ea4a9b6d439196a94310d2
Alejandro Macias <alex.mac.fon@gmail.com>
2024-12-05 16:29:05 -0600

    fix: changed font size from 50 to 22

--- nixosModules/packages/st/config.def.h
+++ nixosModules/packages/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Caskaydia Cove Nerdfont:pixelsize=50:antialias=true:autohint=true";
+static char *font = "Caskaydia Cove Nerdfont:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
