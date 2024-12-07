04c19ff39cf08a1c3cdd8234c5d6ab753de0beb8
Alejandro Macias <alex.mac.fon@gmail.com>
2024-12-07 10:33:45 -0600

    fix: added sufix

--- homeUsers/jmacias8075/scripts/daily-comms.nix
+++ homeUsers/jmacias8075/scripts/daily-comms.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "daily-comms" ''
 ${pkgs.brave}/bin/brave --new-window mail.google.com &
-${pkgs.signal}/bin/signal &
+${pkgs.signal-desktop}/bin/signal-desktop &
 ${pkgs.whatsapp-for-linux}/bin/whatsapp-for-linux &
 ${pkgs.slack}/bin/slack &
 ''
